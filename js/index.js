fadb203d9db21d4f17bbbf30889d74dd210a4bbb
MiZentUi <mizentui@gmail.com>
2024-09-01 21:57:54 +0300

    edit

--- js/index.js
+++ js/index.js
@@ -4,19 +4,19 @@ function render_vacancies(json) {
         vacancies += `
             <li onclick="open_vacancy(${index});">
                 <h3 class="project">${item["Проект КСП Стеклозавод Орехово-Зуево"]}</h5>
-                <span class="job">Должность: ${item["Должность Упаковщик/Разнорабочий"]}</span>
+                <span class="job">Должность</b>: ${item["Должность Упаковщик/Разнорабочий"]}</span>
                 <br>
-                <span class="age">Возраст: ${item["Возраст от 18 до 55 (старше по согласованию)"]}</span>
+                <span class="age">Возраст</b>: ${item["Возраст от 18 до 55 (старше по согласованию)"]}</span>
                 <br>
-                <span class="city">Город: ${item["Город Мос.обл, г. Орехово-Зуево"]}</span>
+                <span class="city">Город</b>: ${item["Город Мос.обл, г. Орехово-Зуево"]}</span>
             </li>
-        `;
+        <br>`;
     });
     const html_vacancies_list = `
         <ul>
             ${vacancies}
         </ul>
-    `;
+    <br>`;
     document.getElementById("vacancies").innerHTML = html_vacancies_list;
 }
 
@@ -24,21 +24,21 @@ function render_vacancy(index) {
     let vacancy_div = document.getElementById("vacancy")
     let vacancy_div_html = `
         <div id="vacancy_card">
-            <h1 id="project">${json[index]["Проект КСП Стеклозавод Орехово-Зуево"]}</h1>
-            <span id="job">Город: ${json[index]["Город Мос.обл, г. Орехово-Зуево"]}</span>
-            <span id="job">Общая потребность: ${json[index]["Общая потребность 8 только день"]}</span>
-            <span id="job">М: ${json[index]["М "]}</span>
-            <span id="job">Ж: ${json[index]["Ж "]}</span>
-            <span id="job">Возраст: ${json[index]["Возраст от 18 до 55 (старше по согласованию)"]}</span>
-            <span id="job">Гражданство: ${json[index]["Гражданство РФ,Кирзизия, Казахстан, РБ,"]}</span>
-            <span id="job">Должность: ${json[index]["Должность Упаковщик/Разнорабочий"]}</span>
-            <span id="job">Адрес: ${json[index]["Адрес Адрес объекта: МО, г. Орехово-Зуево, проезд Заготзерно, 10. Адрес общежития ДЛЯ РФ (без прописки или вр.рег. не заселят)"]}</span>
-            <span id="job">График: ${json[index]["График Вахта:15/30/45/60/90/120 смен. График 6/1, 7/0, ДЕНЬ/НОЧЬ! смены с 08:00 до 20:00 или с 20:00 до 08:00. Местные - 2/2, 6/1, 7/0. ДЕНЬ/НОЧЬ! смены с 08:00 до 20:00 или с 20:00 до 08:00"]}</span>
-            <span id="job">Оплата за смену ВАХТА: ${json[index]["Оплата за смену ВАХТА с 05.08.24 за смену 3500р ( СТАЖИРОВОЧНАЯ СМЕНА ОПЛАЧИВАЕТСЯ, ЕСЛИ КАНДИДАТ ОТРАБАТЫВАЕТ МИНИМУМ 6 СМЕН)"]}</span>
-            <span id="job">Оплата за смену МЕСТНЫЕ: ${json[index]["Оплата за смену МЕСТНЫЕ С 05.08.24 за смену 3500р СТАЖИРОВОЧНАЯ СМЕНА ОПЛАЧИВАЕТСЯ, ЕСЛИ КАНДИДАТ ОТРАБАТЫВАЕТ МИНИМУМ 6 СМЕН)"]}</span>
-            <span id="job">Частота выплат: ${json[index]["Частота выплат Еженедельная без отсрочки"]}</span>
-        </div>
-    `
+            <h1 id="project">${json[index]["Проект КСП Стеклозавод Орехово-Зуево"]}</h1>`;
+    if (json[index]["Город Мос.обл, г. Орехово-Зуево"] != undefined) vacancy_div_html += `<span><b>Город</b>: ${json[index]["Город Мос.обл, г. Орехово-Зуево"]}</span><br>`;
+    if (json[index]["Общая потребность 8 только день"] != undefined) vacancy_div_html += `<span><b>Общая потребность</b>: ${json[index]["Общая потребность 8 только день"]}</span><br>`;
+    if (json[index]["М "] != undefined) vacancy_div_html += `<span><b>М</b>: ${json[index]["М "]}</span><br>`;
+    if (json[index]["Ж "] != undefined) vacancy_div_html += `<span><b>Ж</b>: ${json[index]["Ж "]}</span><br>`;
+    if (json[index]["Возраст от 18 до 55 (старше по согласованию)"] != undefined) vacancy_div_html += `<span><b>Возраст</b>: ${json[index]["Возраст от 18 до 55 (старше по согласованию)"]}</span><br>`;
+    if (json[index]["Гражданство РФ,Кирзизия, Казахстан, РБ,"] != undefined) vacancy_div_html += `<span><b>Гражданство</b>: ${json[index]["Гражданство РФ,Кирзизия, Казахстан, РБ,"]}</span><br>`;
+    if (json[index]["Должность Упаковщик/Разнорабочий"] != undefined) vacancy_div_html += `<span><b>Должность</b>: ${json[index]["Должность Упаковщик/Разнорабочий"]}</span><br>`;
+    if (json[index]["Адрес Адрес объекта: МО, г. Орехово-Зуево, проезд Заготзерно, 10. Адрес общежития ДЛЯ РФ (без прописки или вр.рег. не заселят)"] != undefined) vacancy_div_html += `<span><b>Адрес</b>: ${json[index]["Адрес Адрес объекта: МО, г. Орехово-Зуево, проезд Заготзерно, 10. Адрес общежития ДЛЯ РФ (без прописки или вр.рег. не заселят)"]}</span><br>`;
+    if (json[index]["График Вахта:15/30/45/60/90/120 смен. График 6/1, 7/0, ДЕНЬ/НОЧЬ! смены с 08:00 до 20:00 или с 20:00 до 08:00. Местные - 2/2, 6/1, 7/0. ДЕНЬ/НОЧЬ! смены с 08:00 до 20:00 или с 20:00 до 08:00"] != undefined) vacancy_div_html += `<span><b>График</b>: ${json[index]["График Вахта:15/30/45/60/90/120 смен. График 6/1, 7/0, ДЕНЬ/НОЧЬ! смены с 08:00 до 20:00 или с 20:00 до 08:00. Местные - 2/2, 6/1, 7/0. ДЕНЬ/НОЧЬ! смены с 08:00 до 20:00 или с 20:00 до 08:00"]}</span><br>`;
+    if (json[index]["Оплата за смену ВАХТА с 05.08.24 за смену 3500р ( СТАЖИРОВОЧНАЯ СМЕНА ОПЛАЧИВАЕТСЯ, ЕСЛИ КАНДИДАТ ОТРАБАТЫВАЕТ МИНИМУМ 6 СМЕН)"] != undefined) vacancy_div_html += `<span><b>Оплата за смену ВАХТА</b>: ${json[index]["Оплата за смену ВАХТА с 05.08.24 за смену 3500р ( СТАЖИРОВОЧНАЯ СМЕНА ОПЛАЧИВАЕТСЯ, ЕСЛИ КАНДИДАТ ОТРАБАТЫВАЕТ МИНИМУМ 6 СМЕН)"]}</span><br>`;
+    if (json[index]["Оплата за смену МЕСТНЫЕ С 05.08.24 за смену 3500р СТАЖИРОВОЧНАЯ СМЕНА ОПЛАЧИВАЕТСЯ, ЕСЛИ КАНДИДАТ ОТРАБАТЫВАЕТ МИНИМУМ 6 СМЕН)"] != undefined) vacancy_div_html += `<span><b>Оплата за смену МЕСТНЫЕ</b>: ${json[index]["Оплата за смену МЕСТНЫЕ С 05.08.24 за смену 3500р СТАЖИРОВОЧНАЯ СМЕНА ОПЛАЧИВАЕТСЯ, ЕСЛИ КАНДИДАТ ОТРАБАТЫВАЕТ МИНИМУМ 6 СМЕН)"]}</span><br>`;
+    if (json[index]["Частота выплат Еженедельная без отсрочки"] != undefined) vacancy_div_html += `<span><b>Частота выплат</b>: ${json[index]["Частота выплат Еженедельная без отсрочки"]}</span><br>`;
+    vacancy_div_html = vacancy_div_html.substring(0, vacancy_div_html.length - 1 - 4);
+    vacancy_div_html += `</div>`;
     vacancy_div.innerHTML = vacancy_div_html;
 }
 
@@ -49,6 +49,9 @@ function open_vacancy(vacancy_index) {
     render_vacancy(vacancy_index);
     vacancy_div.style.display = "block";
     window.Telegram.WebApp.BackButton.show();
+    window.Telegram.WebApp.MainButton.text = "Оставить заявку";
+    window.Telegram.WebApp.MainButton.color = "#ff5e00";
+    window.Telegram.WebApp.MainButton.show();
     window.Telegram.WebApp.BackButton.onClick(back_to_main);
 }
 
@@ -58,6 +61,7 @@ function back_to_main() {
     main.style.display = "block";
     vacancy_div.style.display = "none";
     window.Telegram.WebApp.BackButton.hide();
+    window.Telegram.WebApp.MainButton.hide();
 }
 
 window.onload = () => {
